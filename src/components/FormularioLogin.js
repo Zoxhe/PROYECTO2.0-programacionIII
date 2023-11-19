1be4b00d27a382c19b1f3feb9eb1f0825d92c7be
victoria <you@example.comsassoonvictoria@gmail.com>
2023-11-19 16:21:15 -0300

    registro y login terminado

--- src/components/FormularioLogin.js
+++ src/components/FormularioLogin.js
@@ -1,7 +1,5 @@
 import React, { Component } from 'react';
-
 import { View, Text, TouchableOpacity, TextInput, StyleSheet } from "react-native";
-
 import { auth } from "../firebase/config";
 
 class FormularioLogin extends Component {
@@ -10,7 +8,7 @@ class FormularioLogin extends Component {
         this.state = {
             email: "",
             password: ""
-        }
+        };
     }
 
     iniciarSesion(email, password) {
@@ -19,40 +17,81 @@ class FormularioLogin extends Component {
                 this.setState({
                     email: "",
                     password: ""
-                })
+                });
                 this.props.navigation.navigate("NavegacionTab");
             })
             .catch((error) => {
                 console.log(error);
-            })
+            });
     }
 
     render() {
-        return(
-            <View>
-                <Text>Iniciar sesión</Text>
-                <Text>Correo electrónico</Text>
-                <TextInput placeholder="Email" onChangeText={(email) => this.setState({ email })} />
-                <Text>Contraseña</Text>
-                <TextInput placeholder="Password" onChangeText={(password) => this.setState({ password })} />
+        return (
+            <View style={styles.container}>
+                <Text style={styles.title}>Iniciar sesión</Text>
+                <TextInput
+                    placeholder="Correo electrónico"
+                    style={styles.input}
+                    onChangeText={(email) => this.setState({ email })}
+                />
+                <TextInput
+                    placeholder="Contraseña"
+                    style={styles.input}
+                    onChangeText={(password) => this.setState({ password })}
+                    secureTextEntry
+                />
                 <TouchableOpacity onPress={() => this.iniciarSesion(this.state.email, this.state.password)}>
-                    <Text>Iniciar sesión</Text>
+                    <View style={styles.button}>
+                        <Text style={styles.buttonText}>Iniciar sesión</Text>
+                    </View>
                 </TouchableOpacity>
                 <Text>
                     ¿Todavía no tenés una cuenta?
                     <TouchableOpacity onPress={() => this.props.navigation.navigate("Registro")}>
-                        <Text> Registrarte</Text>
+                        <Text style={styles.link}> Registrarte</Text>
                     </TouchableOpacity>
                 </Text>
             </View>
-        )
+        );
     }
 }
 
-const Style = StyleSheet.create({
-    contenedor: {
+const styles = StyleSheet.create({
+    container: {
         flex: 1,
-    }
+        padding: 20,
+        backgroundColor: 'white',
+        justifyContent: 'center',
+    },
+    title: {
+        fontSize: 24,
+        fontWeight: 'bold',
+        marginBottom: 20,
+        textAlign: 'center',
+    },
+    input: {
+        height: 40,
+        borderColor: 'gray',
+        borderWidth: 1,
+        marginBottom: 10,
+        paddingHorizontal: 10,
+        backgroundColor: '#f0f0f0',
+        borderRadius: 5,
+    },
+    button: {
+        backgroundColor: '#3498db',
+        padding: 10,
+        borderRadius: 5,
+        alignItems: 'center',
+        marginTop: 10,
+    },
+    buttonText: {
+        color: 'white',
+        fontSize: 16,
+    },
+    link: {
+        color: '#3498db',
+    },
 });
 
 export default FormularioLogin;
--- src/components/FormularioRegistro.js
+++ src/components/FormularioRegistro.js
@@ -1,89 +1,186 @@
 import React, { Component } from 'react';
-
-import { View, Text, TouchableOpacity, TextInput, StyleSheet } from "react-native";
-
-import { auth, db } from "../firebase/config";
+import { View, Text, TouchableOpacity, TextInput, StyleSheet } from 'react-native';
+import { auth, db } from '../firebase/config';
+import Camara from './Camara';
 
 class FormularioRegistro extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            email: "",
-            password: "",
-            nombre: "",
-            miniBio: "",
-            fotoPerfil: ""
-        }
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: '',
+      password: '',
+      nombre: '',
+      miniBio: '',
+      fotoPerfil: '',
+      mostrarCamara: false,
+    };
+  }
 
-    registrarse(email, password, nombre, miniBio, fotoPerfil) {
-        // COLECCION A USAR --> "users" (SOLO PARA miniBio y fotoPerfil [la URL, la imagen se guarda en storage])
-        auth.createUserWithEmailAndPassword(email, password)
-            .then((data) => {
-                db.collection("users").doc(data.user.uid).set({
-                    email: email,
-                    nombre: nombre,
-                    miniBio: miniBio,
-                    fotoPerfil: fotoPerfil
-                })
-                .then(() => {
-                    console.log("Usuario registrado");
-                    this.setState({
-                        email: "",
-                        password: "",
-                        nombre: "",
-                        miniBio: "",
-                        fotoPerfil: ""
-                    })
-                })
-                .catch((error) => {
-                    console.log(error);
-                })
-            })
-            .catch((error) => {
-                console.log(error);
-            })
-    }
+  registrarse(email, password, nombre, miniBio, fotoPerfil) {
+    auth
+      .createUserWithEmailAndPassword(email, password)
+      .then((data) => {
+        db.collection('users')
+          .doc(data.user.uid)
+          .set({
+            email: email,
+            nombre: nombre,
+            miniBio: miniBio,
+            fotoPerfil: fotoPerfil,
+          })
+          .then(() => {
+            console.log('Usuario registrado');
+            this.setState({
+              email: '',
+              password: '',
+              nombre: '',
+              miniBio: '',
+              fotoPerfil: '',
+            });
+          })
+          .catch((error) => {
+            console.log(error);
+          });
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
 
-    render() {
-        return(
-            <View>
-                <Text>Registrarse</Text>
-                
-                <TextInput placeholder="Email" onChangeText={(email) => this.setState({ email })} />
+  onImageUpload(url) {
+    this.setState({
+      fotoPerfil: url,
+      mostrarCamara: false,
+    });
+  }
 
-               
-                <TextInput placeholder="Password" onChangeText={(password) => this.setState({ password })} />
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.title}>Inicia sesion</Text>
 
-               
-                <TextInput placeholder="Usuario" onChangeText={(nombre) => this.setState({ nombre })} />
+        <TextInput
+          style={styles.input}
+          placeholder="Email"
+          onChangeText={(email) => this.setState({ email })}
+        />
 
-                
-                <TextInput placeholder="Bio" onChangeText={(miniBio) => this.setState({ miniBio })} />
-                
+        <TextInput
+          style={styles.input}
+          placeholder="Password"
+          secureTextEntry
+          onChangeText={(password) => this.setState({ password })}
+        />
 
-                <TextInput placeholder="Foto de perfil" onChangeText={(fotoPerfil) => this.setState({ fotoPerfil })} />
+        <TextInput
+          style={styles.input}
+          placeholder="Username"
+          onChangeText={(nombre) => this.setState({ nombre })}
+        />
 
+        <TextInput
+          style={styles.input}
+          placeholder="Bio"
+          onChangeText={(miniBio) => this.setState({ miniBio })}
+        />
 
-                <TouchableOpacity onPress={() => this.registrarse(this.state.email, this.state.password, this.state.nombre, this.state.miniBio, this.state.fotoPerfil)}>
-                    <Text>Registrarse</Text>
-                </TouchableOpacity>
-                <Text>
-                    ¿Ya tienes una cuenta?
-                    <TouchableOpacity onPress={() => this.props.navigation.navigate("Login")}>
-                        <Text> Iniciá sesión</Text>
-                    </TouchableOpacity>
-                </Text>
-            </View>
-        )
-    }
+        {this.state.mostrarCamara ? (
+          <View>
+            <Camara onImageUpload={(url) => this.onImageUpload(url)} style={{ width: "50vh", height: "50vh", alignItems: 'center' }} />
+          </View>
+        ) : (
+          <TouchableOpacity onPress={() => this.setState({ mostrarCamara: true })}>
+            <Text style={styles.uploadPhotoButton}>Subir foto para tu perfil</Text>
+          </TouchableOpacity>
+        )}
+
+        {this.state.email === "" || this.state.password === "" || this.state.nombre === "" ? (
+          <TouchableOpacity disabled={true} style={styles.buttonDisabled}>
+            <Text style={styles.buttonText}>Registrarme</Text>
+          </TouchableOpacity>
+        ) : (
+          <TouchableOpacity
+            style={styles.button}
+            onPress={() =>
+              this.registrarse(
+                this.state.email,
+                this.state.password,
+                this.state.nombre,
+                this.state.miniBio,
+                this.state.fotoPerfil
+              )
+            }>
+            <Text style={styles.buttonText}>Registrarme</Text>
+          </TouchableOpacity>
+        )}
+
+        <Text style={styles.loginText}>
+          ¿Ya tenes una cuenta? 
+          <TouchableOpacity onPress={() => this.props.navigation.navigate('Login')}>
+            <Text style={styles.linkText}> Iniciá Sesion</Text>
+          </TouchableOpacity>
+        </Text>
+      </View>
+    );
+  }
 }
 
-const Style = StyleSheet.create({
-    contenedor: {
-        flex: 1,
-    },
-    
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 20,
+    backgroundColor: 'white',
+    justifyContent: 'center',
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginBottom: 20,
+    textAlign: 'center',
+  },
+  input: {
+    height: 40,
+    borderColor: 'gray',
+    borderWidth: 1,
+    marginBottom: 10,
+    paddingHorizontal: 10,
+  },
+  button: {
+    backgroundColor: '#3498db', 
+    padding: 10,
+    borderRadius: 5,
+    alignItems: 'center',
+    marginTop: 10,
+  },
+  buttonDisabled: {
+    backgroundColor: '#D8D8D8',
+    padding: 10,
+    borderRadius: 5,
+    alignItems: 'center',
+    marginTop: 10,
+  },
+  buttonText: {
+    color: 'white',
+    fontSize: 16,
+  },
+  loginText: {
+    marginTop: 20,
+    textAlign: 'center',
+  },
+  linkText: {
+    color: 'blue',
+    textDecorationLine: 'underline',
+  },
+  uploadPhotoButton: {
+    fontFamily: 'Courier',
+    fontSize: 14,
+    textAlign: 'center',
+    margin: 10,
+    backgroundColor: '#E1306C', 
+    borderRadius: 10,
+    padding: 10,
+    color: 'white', 
+  },
 });
 
 export default FormularioRegistro;
